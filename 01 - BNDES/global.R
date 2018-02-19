3ffc4f081e1be62a506e852e6efe93869f92107c
Rafael Morais <rafa.unb.2012@gmail.com>
2018-02-18 23:32:36 -0300

    About ok

--- 01 - BNDES/global.R
+++
@@ -1,5 +0,0 @@
-# library(dplyr)
-# load("./data/Shiny_Data.RData")
-# 
-# BNDE <- BNDES %>% group_by(UF, Sector, Year) %>% summarise(freq  = sum(freq),
-#                                                            value = sum(value)/1000000)
